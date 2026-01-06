3ed70ca02b9a060a5de1cf5b3041a2f72c480edb
I54m <I54m@I54m.com>
2026-01-06 20:05:51 +1300

    Add xmlrunner to output unittest files for jenkins

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,16 @@
 from django.core.management import call_command
 from boot_django import boot_django
+import os, sys, xmlrunner
 
 boot_django()
-call_command("test", "i54m_apiuser")
+
+results_dir = os.path.join(os.getcwd(), "test-results")
+os.makedirs(results_dir, exist_ok=True)
+
+call_command("test",
+    verbosity=2,
+    failfast=False,
+    stdout=sys.stdout,
+    stderr=sys.stderr,
+    test_runner='xmlrunner.extra.djangotestrunner.XMLTestRunner',
+    output_dir=results_dir)
